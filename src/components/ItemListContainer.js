30d1b3bf377a7a319ec125a03e34cd7227279f99
maulul <maulul@gmail.com>
2021-07-15 20:34:20 -0300

    promises

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -6,53 +6,51 @@ import { useRef } from 'react'
 
 export default function ProductContainer() {
 
-    const arrayDeProductos = [
-        {
-            name:'Logo de Zelda',
-            imagen:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/The_Legend_of_Zelda.svg/526px-The_Legend_of_Zelda.svg.png',
-            desc:'es el logo principal',
-            precio:500
-            
-        },        
 
-        {
-            name:'espada',
-            imagen:'https://studio.cults3d.com/_8__EoH_s7cGKjbZ9gO7sz1jfG8=/https://files.cults3d.com/uploaders/14052590/illustration-file/ebd27f55-4a72-43ed-8d15-88d1a1a7e6d2/33791.png',
-            desc:'master sword',
-            precio:450
-        }
-        
-    ]
+    const [products,setProducts] = useState([]);
+    useEffect(() =>{
+        const getProducts = async() =>{
+            const respuestaProductos = await fetch('./JSON/products.json')
+            const data = await respuestaProductos.json()
+            setProducts(data)
 
+        }; 
+
+        setTimeout(() => {
+            getProducts()
+        }, 5000);
+    },[])
+    
+    console.log(products)
     const [cart,setCart] = useState([]);
     const primerRender = useRef(true);
 
     const getProduct = (form,producto) =>{
-        form.preventDefault();
+         form.preventDefault();
         
-        let item = {
+         let item = {
             producto: producto.name,
-            imagen:producto.imagen,
-            precio:producto.precio,
-            quantity:form.target[1].value
-        }
+             imagen:producto.imagen,
+             precio:producto.precio,
+             quantity:form.target[1].value
+         }
 
-        setCart([...cart,item]);
-    }
+         setCart([...cart,item]);
+     }
 
-    useEffect(() =>{
-        if(!primerRender.current){
-            alert('Su item fue agregado al carrito')
+     useEffect(() =>{
+         if(!primerRender.current){
+             alert('Su item fue agregado al carrito')
             
-        } else {
-            primerRender.current = false;
-        }
-    },[cart])
+         } else {
+             primerRender.current = false;
+         }
+     },[cart])
 
     return (
         <div>           
 
-            {arrayDeProductos.map(elemento =>{
+             {products.map(elemento =>{
                 return(
                 <Product 
                 name={elemento.name}
@@ -62,10 +60,10 @@ export default function ProductContainer() {
                 addToCart={getProduct}
                 />
                 )
-            })}
+            })}  
 
             <div className="subtotal">
-                {cart.map(element =>{
+                 {cart.map(element =>{
                     return (
                         <>
                         <h3>Producto</h3>
@@ -78,7 +76,7 @@ export default function ProductContainer() {
                         {element.precio * element.quantity}
                         </>
                     )
-                })}
+                })} 
                 
             </div>
         </div>
